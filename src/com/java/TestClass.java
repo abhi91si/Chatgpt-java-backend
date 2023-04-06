04a5befc763ea58666016032d1a297a669628f62
hp <hp@LAPTOP-064JSMF1>
2023-04-06 15:55:47 +0530

    first commit

---
+++ src/com/java/TestClass.java
@@ -0,0 +1,85 @@
+package com.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi this is my java program");
+		readFile();
+		validateFileIsExcel();
+		validateData();
+		validateDuplicateData();
+		calculateCategoryBasedApprovedClaim();
+		calculateApprovalsPending();
+		calculateClaimsVsApprovals();
+		predictClaimsForNextQuarter();
+
+	}
+
+	/**
+	 * Method to read file
+	 */
+	public static List<ArrayList> readFile() {
+		// will return list of objects from a file
+		return null;
+	}
+
+	/**
+	 * Method to validate if file is having .xls extension
+	 */
+	public static boolean validateFileIsExcel() {
+
+		return Boolean.FALSE;
+	}
+
+	/**
+	 * Method to validate data in file check numbers and datatypes
+	 */
+	public static boolean validateData() {
+
+		return Boolean.FALSE;
+	}
+
+	/**
+	 * Method to validate duplicate data present in file
+	 */
+	public static boolean validateDuplicateData() {
+
+		return Boolean.FALSE;
+	}
+
+	/**
+	 * Method to
+	 */
+	public static int calculateCategoryBasedApprovedClaim() {
+
+		return 0;
+	}
+
+	/**
+	 * Method to
+	 */
+	public static int calculateApprovalsPending() {
+
+		return 0;
+	}
+
+	/**
+	 * Method to
+	 */
+	public static int calculateClaimsVsApprovals() {
+
+		return 0;
+	}
+
+	/**
+	 * Method to
+	 */
+	public static int predictClaimsForNextQuarter() {
+
+		return 0;
+	}
+}
